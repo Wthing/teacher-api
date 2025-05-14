39cd1aaf5665ee910c7103fdf9f4ee134254d62a
Wthing <wierd8things@gmail.com>
2025-05-14 13:42:04 +0500

    tweaks

--- views/site/profile.php
+++ views/site/profile.php
@@ -1,8 +1,10 @@
 <?php
+
+use app\models\Form;
 use yii\helpers\Html;
 use yii\helpers\Json;
 
-/** @var \app\models\Form[] $forms */
+/** @var Form[] $forms */
 ?>
 
 <div class="container mt-3">
@@ -21,7 +23,7 @@ use yii\helpers\Json;
         }
         ?>
 
-        <h3 class="mt-4">Данные формы: <?= Html::encode($form->form_name) ?></h3>
+        <h3 class="mt-4"><?= Html::encode($form->form_name) ?></h3>
 
         <!-- Кнопка "Добавить новую запись" -->
         <div class="col-md-12 text-right mb-3">
@@ -41,9 +43,15 @@ use yii\helpers\Json;
             foreach ($formFields as $field) {
                 $fieldId = $field->id;
                 $value = $fieldDataMap[$fieldId][$i]['data'] ?? $fieldDataMap[$fieldId][$i] ?? '';
-                $rowDisplay[] = Html::encode(is_array($value) ? implode(', ', $value) : $value);
-                $rowData[$fieldId] = $value;
+                if ($field->type_id == 5 && is_string($value) && $value !== '') {
+                    // путь к файлу
+                    $url = Html::a(basename($value), Yii::getAlias('@web') . '/' . ltrim($value, '/'), ['target' => '_blank']);
+                    $rowDisplay[] = $url;
+                } else {
+                    $rowDisplay[] = Html::encode(is_array($value) ? implode(', ', $value) : $value);
+                }
 
+                $rowData[$fieldId] = $value;
                 if (isset($fieldDataMap[$fieldId][$i]['id'])) {
                     $recordIds[] = $fieldDataMap[$fieldId][$i]['id'];
                 }
@@ -66,58 +74,49 @@ use yii\helpers\Json;
                             data-ids='<?= Json::encode($recordIds) ?>'>
                         🗑
                     </button>
-
                 </div>
             </div>
         <?php endfor; ?>
     <?php endforeach; ?>
 </div>
 
-<!-- Модальное окно для редактирования -->
-<div class="modal fade" id="editFieldModal" tabindex="-1" role="dialog" aria-labelledby="editFieldModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+<!-- Модальное окно редактирования -->
+<div class="modal fade" id="editFieldModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="editFieldModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <form id="editForm" method="post" action="/site/update-form-data">
+            <form id="editForm" method="post" enctype="multipart/form-data" action="/site/update-form-data">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="editFieldModalLabel">Редактировать данные</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Закрыть">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    <h5 class="modal-title">Редактировать данные</h5>
+                    <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
                 </div>
-                <div class="modal-body" id="modalFieldsContainer">
-                    <!-- Поля вставляются через JS -->
-                </div>
+                <div class="modal-body" id="modalFieldsContainer"></div>
                 <input type="hidden" name="form_id" id="modalFormId">
                 <input type="hidden" name="data_ids" id="modalDataIds">
                 <input type="hidden" name="<?= Yii::$app->request->csrfParam ?>" value="<?= Yii::$app->request->getCsrfToken() ?>">
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary">Сохранить</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
 
-<!-- Модальное окно для создания новой записи -->
-<div class="modal fade" id="createFieldModal" tabindex="-1" role="dialog" aria-labelledby="createFieldModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+<!-- Модальное окно создания -->
+<div class="modal fade" id="createFieldModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="createFieldModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <form id="createForm" method="post" action="/site/create-form-data">
+            <form id="createForm" method="post" enctype="multipart/form-data" action="/site/create-form-data">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="createFieldModalLabel">Создать новую запись</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Закрыть">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    <h5 class="modal-title">Создать новую запись</h5>
+                    <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
                 </div>
-                <div class="modal-body" id="createFieldsContainer">
-                    <!-- Динамически вставляемые поля -->
-                </div>
+                <div class="modal-body" id="createFieldsContainer"></div>
                 <input type="hidden" name="form_id" id="createFormId">
                 <input type="hidden" name="<?= Yii::$app->request->csrfParam ?>" value="<?= Yii::$app->request->getCsrfToken() ?>">
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-success">Создать</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
                 </div>
             </form>
         </div>
@@ -185,10 +184,10 @@ $('.delete-field-btn').on('click', function () {
         type: 'POST',
         data: {
             data_ids: JSON.stringify(recordIds),
-            '<?= $csrfParam ?>': '<?= $csrfToken ?>'
+            '$csrfParam': '$csrfToken'
         },
         success: function () {
-            location.reload(); // перезагружаем страницу
+            location.reload();
         },
         error: function () {
             alert('Ошибка при удалении данных');
@@ -196,51 +195,31 @@ $('.delete-field-btn').on('click', function () {
     });
 });
 
-
 function generateInputByType(typeId, fieldId, value) {
     let input;
 
     switch (typeId) {
         case 1: // integer
-            input = $('<input>')
-                .attr('type', 'number')
-                .addClass('form-control')
-                .attr('name', 'field_values[' + fieldId + ']')
-                .val(value);
+            input = $('<input>').attr('type', 'number').addClass('form-control').attr('name', 'field_values[' + fieldId + ']').val(value);
             break;
         case 2: // string
-            input = $('<input>')
-                .attr('type', 'text')
-                .addClass('form-control')
-                .attr('name', 'field_values[' + fieldId + ']')
-                .val(value);
+            input = $('<input>').attr('type', 'text').addClass('form-control').attr('name', 'field_values[' + fieldId + ']').val(value);
             break;
         case 3: // date
-            input = $('<input>')
-                .attr('type', 'date')
-                .addClass('form-control')
-                .attr('name', 'field_values[' + fieldId + ']')
-                .val(value);
+            input = $('<input>').attr('type', 'date').addClass('form-control').attr('name', 'field_values[' + fieldId + ']').val(value);
             break;
         case 4: // url
-            input = $('<input>')
-                .attr('type', 'url')
-                .addClass('form-control')
-                .attr('name', 'field_values[' + fieldId + ']')
-                .val(value);
+            input = $('<input>').attr('type', 'url').addClass('form-control').attr('name', 'field_values[' + fieldId + ']').val(value);
             break;
         case 5: // file
-            input = $('<input>')
-                .attr('type', 'file')
-                .addClass('form-control')
-                .attr('name', 'field_values[' + fieldId + ']');
+            input = $('<input>').attr('type', 'file').addClass('form-control').attr('name', 'field_files[' + fieldId + ']');
+            if (typeof value === 'string' && value.length > 0) {
+                const fileInfo = $('<p>').html('Загружен файл: <a href="/' + value + '" target="_blank">' + value.split('/').pop() + '</a>');
+                return $('<div>').append(fileInfo).append(input);
+            }
             break;
         default:
-            input = $('<input>')
-                .attr('type', 'text')
-                .addClass('form-control')
-                .attr('name', 'field_values[' + fieldId + ']')
-                .val(value);
+            input = $('<input>').attr('type', 'text').addClass('form-control').attr('name', 'field_values[' + fieldId + ']').val(value);
     }
 
     return input;
