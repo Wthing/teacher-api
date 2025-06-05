fc48f1b5f0532a84836cdbbb1c9b59881c0c3d39
Wthing <wierd8things@gmail.com>
2025-06-05 10:04:18 +0500

    Таблицы для подтверждения

---
+++ migrations/m250605_044141_create_form_confirm_person.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+class m250605_044141_create_form_confirm_person extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('form_confirm_person', [
+            'id' => $this->primaryKey(),
+            'profile_id' => $this->integer()->notNull(),
+            'form_id' => $this->integer()->notNull(),
+        ]);
+
+        $this->createIndex('idx_form_confirm_person_profile_id', 'form_confirm_person', 'profile_id');
+        $this->createIndex('idx_form_confirm_person_form_id', 'form_confirm_person', 'form_id');
+
+        $this->addForeignKey('fk_form_confirm_person_profile_id', 'form_confirm_person', 'profile_id', 'profiles', 'id');
+        $this->addForeignKey('fk_form_confirm_person_form_id', 'form_confirm_person', 'form_id', 'forms', 'id');
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_form_confirm_person_profile_id', 'form_confirm_person');
+        $this->dropIndex('idx_form_confirm_person_profile_id', 'form_confirm_person');
+
+        $this->dropForeignKey('fk_form_confirm_person_form_id', 'form_confirm_person');
+        $this->dropIndex('idx_form_confirm_person_form_id', 'form_confirm_person');
+
+        $this->dropTable('form_confirm_person');
+    }
+}
---
+++ migrations/m250605_044810_create_form_confirm_application.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+class m250605_044810_create_form_confirm_application extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('form_confirm_application', [
+            'id' => $this->primaryKey(),
+            'record_index' => $this->integer()->notNull(),
+            'created_by' => $this->integer()->notNull(),
+            'assigned_to' => $this->integer()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'confirmed_at' => $this->integer()->null(),
+            'status' => $this->integer()->notNull()->defaultValue(0),
+        ]);
+
+        $this->createIndex('idx_form_confirm_application_created_by', 'form_confirm_application', 'created_by');
+        $this->createIndex('idx_form_confirm_application_assigned_to', 'form_confirm_application', 'assigned_to');
+
+        $this->addForeignKey('fk_form_confirm_application_created_by', 'form_confirm_application', 'created_by', 'profiles', 'id');
+        $this->addForeignKey('fk_form_confirm_application_assigned_to', 'form_confirm_application', 'assigned_to', 'profiles', 'id');
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_form_confirm_application_created_by', 'form_confirm_application');
+        $this->dropIndex('idx_form_confirm_application_created_by', 'form_confirm_application');
+
+        $this->dropForeignKey('fk_form_confirm_application_assigned_to', 'form_confirm_application');
+        $this->dropIndex('idx_form_confirm_application_assigned_to', 'form_confirm_application');
+
+        $this->dropTable('form_confirm_application');
+    }
+}
