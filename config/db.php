3378a164e928fe06b0ecdac8d44fda856f93c3ae
Wthing <wierd8things@gmail.com>
2025-06-18 14:58:13 +0500

    env но без env

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=db',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'],
+    'username' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASS'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
